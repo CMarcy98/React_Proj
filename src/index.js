b69e15f6c30bebd96aff30374626725ae8b6e1d6
Christian Marcy <christian.marcy@gmail.com>
2018-05-23 16:28:17 -0400

    Added props argument for VideoList Component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAmGBY6J7OK9aUGe726l-e6jlNIIe_crl4';
 
@@ -20,6 +21,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
