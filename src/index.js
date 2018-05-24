6cd6e550c89e210e8092c5a3239e84afe9d9b35e
Christian Marcy <christian.marcy@gmail.com>
2018-05-24 10:13:13 -0400

    Added loading state to allow time for API repsonse

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAmGBY6J7OK9aUGe726l-e6jlNIIe_crl4';
 
@@ -21,6 +22,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
 				<VideoList videos={this.state.videos} />
 			</div>
 		);
